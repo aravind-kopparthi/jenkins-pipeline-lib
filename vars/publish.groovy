d6c846e84cb6d5a1199fc441bab27cb5b5a3ddd6
Satya Aravind Kopparthi <36413278+aravind-kopparthi@users.noreply.github.com>
2022-11-02 11:36:09 -0400

    Update publish.groovy

--- vars/publish.groovy
+++ vars/publish.groovy
@@ -1,1 +1,30 @@
 
+pipeline {
+
+    agent any
+
+    environment {
+
+        JFROG_CLI_BUILD_NAME = "${env.JOB_NAME}"
+
+        JFROG_CLI_BUILD_NUMBER = "${env.BUILD_NUMBER}"
+
+    }
+
+    stages {
+
+        stage ('Run JFrog CLI') {
+
+            steps {
+
+                sh 'jfrog rt mvn -f /path/to/pom.xml clean install' // build & deploy artifacts
+
+                sh 'jfrog rt bp' // publish build info
+
+            }
+
+        }
+
+    }
+
+}
